9dfa9075466d74af90bf4183f9c5fd018f13253f
vagnerseibert <vagnerseibert@gmail.com>
2016-08-27 00:07:14 -0300

    Criado classe ScenesManager, adicionado scenes manager no menu

---
+++ Titan Tower/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Represents a scene and his caller
+/// </summary>
+[System.Serializable]
+public struct SceneSet {
+    public string sceneName;
+    public Button sceneButton;
+}
+
+/// <summary>
+/// Handles the binding of scenes and their callers
+/// </summary>
+public class ScenesManager : MonoBehaviour {
+    public SceneSet[] sceneSet;
+
+    void Start () {
+        foreach(var ss in sceneSet)
+            ss.sceneButton.onClick.AddListener(() => SceneManager.LoadScene(ss.sceneName));
+    }
+}
